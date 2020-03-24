30f83a2ac6f73c303141778f871d6a97191ca3d2
Abhinav Tyagi <61839001+abhinavtyagi262@users.noreply.github.com>
2020-03-24 17:58:31 +0530

    sendRedirect Demo
    Square of (Addition of two numbers) using sendRedirect

---
+++ 3sendRedirect URL Rewriting/src/com/abhinav/AddServlet.java
@@ -0,0 +1,23 @@
+package com.abhinav;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AddServlet extends HttpServlet
+{
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException
+	{
+		int i = Integer.parseInt(req.getParameter("num1"));
+		int j = Integer.parseInt(req.getParameter("num2"));
+		
+		int k = i + j;
+		
+		
+		res.sendRedirect("sq?k="+k);
+		
+		 		
+	}
+}
